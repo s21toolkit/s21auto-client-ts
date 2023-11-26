b34f349ec6a7ded414d466ef598134703ce113cd
bgenia <bgenia@student.21-school.ru>
2023-11-26 22:30:03 +0300

    refactor: fix export order

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * from "@/api"
 export * from "@/auth/core"
 export * from "@/auth/providers"
 export * from "@/Client"
 export * from "@/constants"
 export * from "@/gql"
-export * from "@/api"
